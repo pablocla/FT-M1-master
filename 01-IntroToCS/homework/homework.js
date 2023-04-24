1d702476d660ed4e9591845fdb1af97ab84d6859
Pablocla <pabloclavero03@gmail.com>
2023-04-23 23:29:45 -0300

    ow

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,21 @@
-'use strict';
+function BinarioADecimal(nro = undefined) {
+   if (nro === undefined || typeof nro !== "string" || !/^[01]+$/.test(nro)) {
+     console.error("Datos incorrectos. Por favor, ingrese un número binario válido.");
+     return;
+   }
+   return parseInt(nro, 2);
+ }
+ 
+ function DecimalABinario(nro = undefined) {
+   if (nro === undefined || typeof nro !== "number" || !Number.isInteger(nro) || nro < 0) {
+     console.error("Datos incorrectos. Por favor, ingrese un número entero no negativo válido.");
+     return;
+   }
+   return nro.toString(2);
+ }
+ 
 
-function BinarioADecimal(num) {}
-
-function DecimalABinario(num) {}
-
+ 
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
